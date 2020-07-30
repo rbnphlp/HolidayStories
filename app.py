7d8e0c4246a6be91d35386039ed1aaf7beda2d4c
Robin Philip <rbnphlp@hotmail.com>
2020-07-30 07:28:56 +0000

    Deploying to heroku

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return ("Hello world! Test to deploy on heroku")
+
+
+if __name__=="__main__":
+        app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
